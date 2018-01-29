1d1fb32889da7930d8a477d290931eb7d8584940
ginaliqili <ginali@vt.edu>
2018-01-28 23:26:38 -0700

    comments

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This repository contains the following files and directories:
 
 * **PM25-estimation:** R code to execute the machine learning algorithms that will be used to estimate PM2.5 exposures
 * **download-earth-observations:** Python code to programatically download earth observation data sets necessary for our analysis
-* **config:** Documentation and configuration files for reproducing environment including AWS setup, executing earth observation downloads, etc. 
+* **config:** Documentation and configuration files for reproducing environment including AWS setup, pulling docker image, etc. 
--- download-earth-observations/MODIS_AOD_L2_download.py
+++ download-earth-observations/MODIS_AOD_L2_download.py
@@ -8,12 +8,14 @@ ftp.login()
 directory = 'allData/6/MOD04_L2/'
 ftp.cwd(directory)
 
+# Each year from 2008-2014
 for year in range(2008, 2015):
     year_directory = str(year)
     print(year_directory)
     ftp.cwd(year_directory)
     date_directories = ftp.nlst()
     print(date_directories)
+    # each day in given year
     for date_directory in date_directories:
         print(date_directory)
         ftp.cwd(date_directory)
@@ -21,15 +23,21 @@ for year in range(2008, 2015):
         print(aod_files)
 
         for aod_file in aod_files:
+            print("Now downloading: " + aod_file)
+
+            # If OS is windows
             if os.name == 'nt':
                 local_path = sys.argv[1] + "\\" + year_directory + "\\" + date_directory
+
+            # If OS is linux
             if os.name == 'posix':
-                local_path = sys.argv[1] + "\\" + year_directory + "\\" + date_directory
+                local_path = sys.argv[1] + "/" + year_directory + "/" + date_directory
 
             if not os.path.exists(local_path):
                 os.makedirs(local_path)
                 os.chdir(local_path)
 
+            # Retrive data
             local_filename = os.path.join(local_path, aod_file)
             file = open(local_filename, 'wb')
             ftp.retrbinary('RETR ' + aod_file, file.write)
