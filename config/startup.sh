0389cfc262045097c1553fc3f2312e8f9d67e078
Ubuntu <ubuntu@ip-172-31-15-225.us-west-2.compute.internal>
2018-01-31 08:06:16 +0000

    edited startup

--- config/startup.sh
+++ config/startup.sh
@@ -3,7 +3,8 @@
 docker pull earthlab/r-spatial-aws
 docker run -d -p 8787:8787 earthlab/r-spatial-aws
 name=$(docker ps --format "{{.Names}}")
-echo $name
-docker exec -u rstudio $name aws configure
+docker exec -u rstudio $name aws s3 cp s3://earthlab-reid-group/ /home/rstudio/ --recursive
+docker exec -it -u rstudio $name /bin/bash
+#docker exec -u rstudio $name aws configure
  
  
