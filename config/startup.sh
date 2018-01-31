587b2bd08b8031a075bcb0e6600a73b2962fb95e
Ubuntu <ubuntu@ip-172-31-15-225.us-west-2.compute.internal>
2018-01-31 07:30:37 +0000

    finished startup script

--- config/startup.sh
+++ config/startup.sh
@@ -3,5 +3,7 @@
 docker pull earthlab/r-spatial-aws
 docker run -d -p 8787:8787 earthlab/r-spatial-aws
 name=$(docker ps --format "{{.Names}}")
-docker exec -it $name /bin/bash -c "su - rstudio"
+echo $name
+docker exec -u rstudio $name aws configure
+ 
  
