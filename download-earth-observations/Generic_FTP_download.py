771e2096f31cf915c1490509fbb817c21c35f6dd
Ubuntu <ubuntu@ip-172-31-15-225.us-west-2.compute.internal>
2018-03-06 20:30:26 +0000

    script for downloading data from FTP site to an S3 bucket

--- download-earth-observations/Generic_FTP_download.py
+++ download-earth-observations/Generic_FTP_download.py
@@ -16,14 +16,23 @@ Results: .hdf files for the data set of interest
 
 import ftplib
 import sys, os, re
+import boto
+from boto.s3.key import Key
 
 __credits__ = "Gina Li, Colleen Reid, Melissa Maestas, Ellen Considine"
 __email__ = "gina.li@colorado.edu"
 
+import boto
+from boto.s3.key import Key
+
+keyId = "AKIAJI4EQVEOL3JMBWLA"
+sKeyId= "j4z5C4p+OUhidT19W8ayTU0h08kXJI33hBLcss3M"
 
 
 # argv[1] is the the FTP URL including sub-directories (i.e. "ftp.ncdc.noaa.gov/pub/has/HAS011070577/" make sure to omit the "ftp://" from the beginning)
 # argv[2] is the local file location to write out to
+
+
 
 #Open ftp connection
 
@@ -41,10 +50,13 @@ ftp.login()
 ftp.cwd(sub_directories_str)
 
 data_files = ftp.nlst()
-print(data_files)
+print("Number of files available for download on FTP site:")
 print(len(data_files))
 
+count = 0
 for data_file in data_files:
+
+    print(data_file)
 
     local_path = sys.argv[2]
 
@@ -54,11 +66,28 @@ for data_file in data_files:
 
     # Retrieve data
     local_filename = os.path.join(local_path, data_file)
-    print(local_filename)
-    file = open(local_filename, 'wb')
+    file = open(local_filename, 'wb+')
+    
     ftp.retrbinary('RETR ' + data_file, file.write)
 
-    file.close()
+    bucketName="earthlab-reid-group"
 
+    conn = boto.connect_s3(keyId,sKeyId)
+    bucket = conn.get_bucket(bucketName)
+    #Get the Key object of the bucket
+    k = Key(bucket)
+    #Crete a new key with id as the name of the file
+    k.key="GASP-AOD/" + data_file
+    #Upload the file
+    result = k.set_contents_from_file(file, rewind=True)
+    #result contains the size of the file uploaded
+    count+=1
+
+    # delete the downloaded file locally after uploading to S3
+    os.remove(local_filename)
+
+print("Number of files successfully downloaded from FTP site:")
+print(count)
+print("Done downloading all data sets")
 ftp.quit()
 
