83064b0e4f438e72ee23b27f04eaa8adbeb2cf7e
Melissa May Maestas <mema2636@colorado.edu>
2018-06-06 15:59:37 -0600

    working on NARR data

--- PM25-estimation/Process_NARR_data.R
+++ PM25-estimation/Process_NARR_data.R
@@ -25,9 +25,9 @@ for (this_year in start_study_year:stop_study_year) { # cycle through each year 
     
     # open file
     print("write code to open file")
-    ReadGrib(file.names, levels, variables,
-             forecasts = NULL, domain = NULL, domain.type = "latlon",
-             file.type = "grib2", missing.data = NULL) # see https://cran.r-project.org/web/packages/rNOMADS/rNOMADS.pdf
+    #ReadGrib(file.names, levels, variables,
+    #         forecasts = NULL, domain = NULL, domain.type = "latlon",
+    #         file.type = "grib2", missing.data = NULL) # see https://cran.r-project.org/web/packages/rNOMADS/rNOMADS.pdf
     ReadGrib(file.names = file.path(NARR.directory,this_year,this_file_name), levels = 1, variables = RH,
              forecasts = NULL, domain = NULL, domain.type = "latlon",
              file.type = "grib2", missing.data = NULL) # see https://cran.r-project.org/web/packages/rNOMADS/rNOMADS.pdf
