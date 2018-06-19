0ca87537ba1ef1df445edbdbc82b7680bdd3e88e
Melissa Maestas <Melissa.Maestas@Colorado.edu>
2018-06-19 18:59:49 +0000

    de-bugging

--- PM25-estimation/concatinate_within_column_function.R
+++ PM25-estimation/concatinate_within_column_function.R
@@ -3,8 +3,8 @@
 
 # var_interest <- "Units_of_Measure"
 concatinate_within_column.fn <- function(var_interest, this_day_all_combined_true_dup) {
+  unique_var_values <- unique(this_day_all_combined_true_dup[,var_interest])
   if (length(unique(this_day_all_combined_true_dup[,var_interest]))>1) { # is there more than 1 value?
-    unique_var_values <- unique(this_day_all_combined_true_dup[,var_interest])
     print(unique_var_values)
     for (Var_i in 1:length(unique_var_values)) { # loop through all values and paste them together
       if (Var_i==1) {
@@ -14,7 +14,7 @@ concatinate_within_column.fn <- function(var_interest, this_day_all_combined_tru
       } # if (Var_i==1) {
     } # for (Var_i in 1:dim(unique_var_values)[1]) {
   } else { # if (length(unique(this_day_all_combined_true_dup[,var_interest]))>1) { # is there more than 1 value?
-    all_Vars <- unique(unique_var_values[,var_interest])
+    all_Vars <- unique_var_values # unique(unique_var_values[,var_interest])
   } # if (length(unique(this_day_all_combined_true_dup[,var_interest]))>1) { # is there more than 1 value?
   print(all_Vars)
   return(all_Vars)
