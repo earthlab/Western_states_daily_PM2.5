b29a7fc96fca76535214d164e710d06237150437
Melissa Maestas <Melissa.Maestas@Colorado.edu>
2018-07-10 22:57:44 +0000

    NARR code

--- PM25-estimation/Process_NARR_data.R
+++ PM25-estimation/Process_NARR_data.R
@@ -26,6 +26,29 @@ for (this_year in start_study_year:stop_study_year) { # cycle through each year 
     # find out what model, date, levels and variables are in grib file, see page 20 of https://cran.r-project.org/web/packages/rNOMADS/rNOMADS.pdf
     GribInfo(file.path(NARR.directory,this_year,this_file_name),file.type = "grib1")
     
+    # <- file.path(NARR.directory,this_year,this_file_name)
+    #this_level <- "2 m above ground"
+    pressure <- c(1, 2, 3, 5, 7,
+                  10, 20, 30, 50, 70,
+                  seq(100, 1000, by = 25))
+    this_level <- paste(pressure, " mb", sep = "")
+    this_variable <- "TMP"
+    #forecasts = NULL, domain = NULL, domain.type = "latlon", 
+    #         file.type = "grib1"
+             #, missing.data = NULL
+    #data_list_1_file <- ReadGrib(file.names = file.path(NARR.directory,this_year,this_file_name), levels = 1, variables = "DPT",file.type = "grib1") # see https://cran.r-project.org/web/packages/rNOMADS/rNOMADS.pdf
+    data_list_1_file <- ReadGrib(file.names = file.path(NARR.directory,this_year,this_file_name), levels = this_level, variables = this_variable,file.type = "grib1") # see https://cran.r-project.org/web/packages/rNOMADS/rNOMADS.pdf
+    
+    print(data_list_1_file)
+    
+    MetaData <- data_list_1_file[[1]]
+    DataValue <- data_list_1_file[[2]]
+    DataVariables <- data_list_1_file[[3]]
+    DataLevels <- data_list_1_file[[4]]
+    DataGrbType <- data_list_1_file[[5]]
+    
+    variables_to_process <- c("PRMSL","TMP","DPT","RH","UGRD","VGRD","HPBL","PRES","PRATE")
+    print(variables_to_process)
     
     
     model.parameters <- ParseModelPage(file.path(NARR.directory,this_year,this_file_name))
@@ -37,9 +60,12 @@ for (this_year in start_study_year:stop_study_year) { # cycle through each year 
     #ReadGrib(file.names, levels, variables,
     #         forecasts = NULL, domain = NULL, domain.type = "latlon",
     #         file.type = "grib2", missing.data = NULL) # see https://cran.r-project.org/web/packages/rNOMADS/rNOMADS.pdf
-    File_Data <- ReadGrib(file.names = file.path(NARR.directory,this_year,this_file_name), levels = 1, variables = RH,
-             forecasts = NULL, domain = NULL, domain.type = "latlon",
-             file.type = "grib2", missing.data = NULL) # see https://cran.r-project.org/web/packages/rNOMADS/rNOMADS.pdf
+    
+    load_data <- ReadGrib(file.names = file.path(NARR.directory,this_year,this_file_name), levels = "MSL", variables = "PRMSL") # see https://cran.r-project.org/web/packages/rNOMADS/rNOMADS.pdf
+    
+    #File_Data <- ReadGrib(file.names = file.path(NARR.directory,this_year,this_file_name), levels = 1, variables = RH,
+    #         forecasts = NULL, domain = NULL, domain.type = "latlon",
+    #         file.type = "grib2", missing.data = NULL) # see https://cran.r-project.org/web/packages/rNOMADS/rNOMADS.pdf
     
     # if getting this error: Error in plot.new() : figure margins too large
     # do this:
