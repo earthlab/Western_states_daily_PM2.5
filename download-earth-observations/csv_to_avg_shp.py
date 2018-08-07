895a7f7de22eb718e28087b4d9131280e9a1cb85
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2018-08-07 14:19:32 -0600

    incorporated multiprocessing

--- download-earth-observations/csv_to_avg_shp.py
+++ download-earth-observations/csv_to_avg_shp.py
@@ -2,30 +2,19 @@ import geopandas as gpd
 from shapely.geometry import Point
 import matplotlib.pyplot as plt
 import os, glob, re, csv
+import multiprocessing
 
 origpath = "/home/jovyan/MODIS-AOD/CSV/"  # these are the non-averaged shapefiles, downloaded from s3
 outpath = "/home/jovyan/MODIS-AOD/avg_shp/"  # upload to s3 when finished
 
-# create a list of all the days
-days = []
-for item in os.listdir(origpath):
-    # print(item)
-    this_day = item[10:17]
-    # print(this_day)
-    if (this_day in days):
-        pass
-    else:
-        days.append(this_day)
-
-# calculate average values at each location, on each day
-for day in days:
+def process(day):
     print("Day: " + day)
     vals = dict()
     long = []
     lat = []
     aod = []
     for file in sorted(glob.glob(origpath + '*%s.*.csv' % (day))):
-        with open(origpath + item, 'r') as csvfile: #had to change to 'r' from 'rb'
+        with open(origpath + item, 'r') as csvfile: 
             r = csv.reader(csvfile, delimiter=',')
 
             for i, row in enumerate(r):
@@ -77,3 +66,27 @@ for day in days:
     proj_str = 'PROJCS["USA_Contiguous_Albers_Equal_Area_Conic",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137,298.257222101]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Albers"],PARAMETER["False_Easting",0],PARAMETER["False_Northing",0],PARAMETER["central_meridian",-96],PARAMETER["Standard_Parallel_1",29.5],PARAMETER["Standard_Parallel_2",45.5],PARAMETER["latitude_of_origin",37.5],UNIT["Meter",1]]'
     outfile = outpath + day + '_avg.shp'
     new_SHP.to_file(outfile, driver='ESRI Shapefile', crs_wkt=proj_str)
+
+    
+def main():
+    # create a list of all the days
+    days = []
+    for item in os.listdir(origpath):
+        # print(item)
+        this_day = item[10:17]
+        # print(this_day)
+        if (this_day in days):
+            pass
+        else:
+            days.append(this_day)
+
+    # calculate average values at each location, on each day
+    pool = multiprocessing.Pool()
+    for day in days:
+        pool.apply_async(process, [day])
+    pool.close()
+    pool.join()
+    
+    
+if __name__ == "__main__":
+    main()
