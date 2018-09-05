cec0ccc42828501346c75759b9c03492025c4c14
Ubuntu <ubuntu@ip-172-31-40-41.us-west-2.compute.internal>
2018-09-05 14:56:11 +0000

    gasp review edits

--- download-earth-observations/GASP_processing/main_real.py
+++ download-earth-observations/GASP_processing/main_real.py
@@ -5,6 +5,7 @@
 import glob, os
 import multiprocessing
 import boto.s3.connection
+import argparse
 from boto.s3.key import Key
 
 from step0 import zero
@@ -13,25 +14,30 @@ from step2 import two
 from step3 import three
 from step4a import four
 
-# Setting up AWS S3 Connection
-access_key = ''
-secret_key = ''
+def _setup():
+	parser = argparse.ArgumentParser(description='Pass in AWS credentials.')
+	parser.add_argument('--access_key',
+                    help='aws access key')
+	parser.add_argument('--secret_key', type=str,
+                    help='secret access key')
+	parser.add_argument('--s3_bucket', type=str, help='s3 bucket name')
+	parser.add_argument('--data_directory', type=str, help='directory path for processed output')
+	args = parser.parse_args()
+	print(args.access_key, args.secret_key, args.s3_bucket, args.data_directory)
 
-conn = boto.connect_s3(
-    aws_access_key_id=access_key,
-    aws_secret_access_key=secret_key
-)
-
-s3Bucketname = "earthlab-reid-group"
-
-
-def upload_to_AWS(subdir, file):
-    bucket = conn.get_bucket(s3Bucketname)
+def upload_to_S3(s3_folder_path, file):
+    bucket = conn.get_bucket(args.s3_bucket)
     k = Key(bucket)
-    k.key = subdir + os.path.basename(file)
+    k.key = s3_folder_path + os.path.basename(file)
     k.set_contents_from_filename(file)  # rewind = True if from file
 
 def main():
+    # Connect to AWS
+    conn = boto.connect_s3(
+   	aws_access_key_id = args.access_key,
+	aws_secret_access_key = args.secret_key
+    )	
+    s3_bucket_name = args.s3_bucket	
 
     # SET YEAR
     year = 2009
@@ -39,10 +45,10 @@ def main():
     # #Step 0: OR use gunzip on all files in the folder
     # origpath = 'C:\\Users\\elco2649\\Documents\\GASP_DATA\\' #'/home/jovyan/GASP-AOD/'
     # outpath = 'C:\\Users\\elco2649\\Documents\\GASP_AOD\\step_0\\' #('/home/jovyan/GASP_processed/%d/step0/'  % (year) )
-    #
+    
     # pool = multiprocessing.Pool()
     # for item in os.listdir(origpath):
-    #     pool.apply_async(zero, [origpath, outpath, item])
+    #    pool.apply_async(zero, [origpath, outpath, item])
     # pool.close()
     # pool.join()
     #
@@ -50,21 +56,22 @@ def main():
     # #     os.remove(file)
     #
     #
-    # #Step 1
-    # origpath = 'C:\\Users\\elco2649\\Documents\\GASP_DATA\\' #('/home/jovyan/GASP_processed/%d/step0/'  % (year) )
-    # outpath = 'C:\\Users\\elco2649\\Documents\\GASP_AOD\\step_1\\' #('/home/jovyan/GASP_processed/%d/step1/'  % (year) )
-    # subdir = ("GASP_processed/%d/step0/"  % (year) )
-    #
-    # pool = multiprocessing.Pool()
-    # for item in os.listdir(origpath):
-    #     pool.apply_async(one, [origpath, outpath, item])
-    # pool.close()
-    # pool.join()
-    #
-    # # for file in sorted(glob.glob(origpath + 'GOESW*')):
-    # #     #upload_to_AWS(subdir, file)
-    # #     os.remove(file)
-    #
+
+    #Step 1
+    data_path = args.data_directory + '/%d/step0/'  % (year)
+    out_path = args.data_directory + '/%d/step1/'  % (year)
+    s3_folder_path = '/%d/step0/'  % (year)
+    
+    pool = multiprocessing.Pool()
+    for item in os.listdir(origpath):
+        pool.apply_async(one, [origpath, outpath, item])
+    pool.close()
+    pool.join()
+    
+    for file in sorted(glob.glob(origpath + 'GOESW*')):
+        upload_to_AWS(subdir, file)
+        os.remove(file)
+
     # # Step 2
     # origpath = 'C:\\Users\\elco2649\\Documents\\GASP_AOD\\step_1\\' # ('/home/jovyan/GASP_processed/%d/step1/' % (year))
     # outpath = 'C:\\Users\\elco2649\\Documents\\GASP_AOD\\step_2\\' #('/home/jovyan/GASP_processed/%d/step2/' % (year))
@@ -134,6 +141,8 @@ def main():
     for file in sorted(glob.glob(origpath + '*')):
         upload_to_AWS(subdir, file)
         os.remove(file)
+'''
 
 if __name__ == "__main__":
-    main()
+    	_setup()
+	#main()
