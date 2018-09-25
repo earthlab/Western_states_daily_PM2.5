428d97e093f19a1a32a4b702b31af5b106dfc513
Gina Li <gina.li@colorado.edu>
2018-09-25 15:36:21 -0600

    added tiles and month end list

--- download-earth-observations/MODIS_HTTPS_download.py
+++ download-earth-observations/MODIS_HTTPS_download.py
@@ -2,24 +2,30 @@
 
 from urllib2 import urlopen, URLError, HTTPError
 import json
+import calendar
 
 import boto
 from boto.s3.key import Key
 import os
+import argparse
 
 class HTTPSDownloader:
     def __init__(self):
         args = self._setup()
+        
         self.start_year = args.start_year
         self.end_year = args.end_year
         self.access_key = args.access_key
         self.secret_key = args.secret_key
         self.s3_bucket = args.s3_bucket
-        self.data_directory = args.data_directory
         self.output_path = args.output_path
-        
+        self.collection_number = args.collection_number
+        self.data_set_name = args.data_set_name
+        #self.tiles = args.tiles
+        self.tiles = ['h08v04', 'h09v04', 'h08v05', 'h10v04', 'h11v04', 'h09v05', 'h10v05']
+
         self.conn = boto.connect_s3(
-            aws_access_key=self.access_key,
+            aws_access_key_id=self.access_key,
             aws_secret_access_key=self.secret_key
         )
 
@@ -38,23 +44,25 @@ class HTTPSDownloader:
             parser.add_argument('--collection_number', type=str, required=True,
                                 help='collection number as it appears in NASA LAADS DAAC site i.e. 61 for colelction 6.1')
             parser.add_argument('--s3_bucket', type=str, required=True, help='s3 bucket name')
-            parser.add_argument('--data_directory', type=str, required=True, help='directory path where data is stored, including lat and lon files')
             parser.add_argument('--output_path', type=str, required=True,
                                 help='path for saving out data')
-'''
-output_path = "C:\Users\User\Documents\MA\RA_2018\collected_data\MODIS_AOD\\"
-start_year = 2008
-end_year = 2014
-data_set_name = "MOD04_L2"
-# Note that collection 6.1 is now available, so can change this to 61
-collection_number = 6
+            #parser.add_argument('--tiles', nargs='+', help='MODIS sinusoidal grid tiles to download')
+            args = parser.parse_args()
+            return args
+    '''
+    output_path = "C:\Users\User\Documents\MA\RA_2018\collected_data\MODIS_AOD\\"
+    start_year = 2008
+    end_year = 2014
+    data_set_name = "MOD04_L2"
+    # Note that collection 6.1 is now available, so can change this to 61
+    collection_number = 6
 
-# Amazon Key ID and Secret Key ID
-keyId = ""
-sKeyId = ""
-bucketName = "earthlab-reid-group"
-subdir = "MODIS-AOD/collected_data/"
-'''
+    # Amazon Key ID and Secret Key ID
+    keyId = ""
+    sKeyId = ""
+    bucketName = "earthlab-reid-group"
+    subdir = "MODIS-AOD/collected_data/"
+    '''
     def dlfile(self, url, hdf_filename):
         # Open the url
         try:
@@ -62,7 +70,7 @@ subdir = "MODIS-AOD/collected_data/"
             print "downloading " + url
 
             # Open our local file for writing
-            with open(output_path + hdf_filename, "wb+") as local_file:
+            with open(self.output_path + hdf_filename, "wb+") as local_file:
                 local_file.write(f.read())
                 #uploadToS3Bucket(hdf_filename, local_file, subdir)
 
@@ -74,44 +82,33 @@ subdir = "MODIS-AOD/collected_data/"
 
 
     def uploadToS3Bucket(self, hdf_filename, file, subdir):
-        conn = boto.connect_s3(keyId, sKeyId)
-        bucket = conn.get_bucket(bucketName)
+        conn = boto.connect_s3(self.access_key, self.secret_key)
+        bucket = conn.get_bucket(self.s3_bucket)
         # Get the Key object of the bucket
         k = Key(bucket)
         # Crete a new key with id as the name of the file
         k.key = subdir + hdf_filename
         # Upload the file
         result = k.set_contents_from_file(file, rewind=True)
-        os.remove(output_path + hdf_filename)
+        os.remove(self.output_path + hdf_filename)
 
 
-    def isLeapYear(self, year):
-        if year%4 == 0:
-            if year%100 == 0:
-            if year%400 == 0:
-                return True
-            else:
-                return False
-            else:
-                return True
-        else:
-            return False
-
-
-    def main():
+    def main(self):
         # Iterate over years of interest
         for year in range(self.start_year, self.end_year+1):
-            if self.isLeapYear(year):
+            if calendar.isleap(year):
                 end_date = 366
+                month_end_list = [1, 32, 61, 92, 122, 153, 183, 214, 245, 275, 306, 336]
             else:
                 end_date = 365
+                month_end_list = [1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
             # Iterate over all dates in year
-            for julian_day in range(1, self.end_date+1):
+            for julian_day in month_end_list:
                 print("Downloading data sets for year " + str(year) + " and julian day " + str(julian_day))
                 julian_day = str(julian_day).zfill(3)
                 # construct base URL with correct year and date
-                base_url = ("https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/%d/%s/%d/%s" %
-                    (collection_number, data_set_name, year, julian_day))
+                base_url = ("https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/%s/%s/%d/%s" %
+                    (self.collection_number, self.data_set_name, year, julian_day))
                 # construct link to json file with list of all HDF files for a given date
                 hdf_list = base_url + ".json"
                 response = urlopen(hdf_list)
@@ -122,11 +119,13 @@ subdir = "MODIS-AOD/collected_data/"
                 # Get the name of each HDF file
                 for j in parsed_json:
                     hdf_filename = j['name']
-                    print("hdf_filename: " + hdf_filename)
-                    url = base_url + "/" + hdf_filename
-                    dlfile(url, hdf_filename)
+                    tile = hdf_filename.split(".")[2]
+                    if tile in self.tiles:
+                        print("hdf_filename: " + hdf_filename)
+                        url = base_url + "/" + hdf_filename
+                        self.dlfile(url, hdf_filename)
 
 
 
 if __name__ == '__main__':
-    main()
+    HTTPSDownloader().main()
