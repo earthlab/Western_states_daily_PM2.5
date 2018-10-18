06f92464742517a057e2f73dad378d32548ef8d3
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2018-10-18 10:55:22 -0600

    Updated with structure of monitor locations

--- download-earth-observations/Highways/R_highways.R
+++ download-earth-observations/Highways/R_highways.R
@@ -3,13 +3,14 @@ library(rgdal)
 library(sp)
 library(rgeos)
 
-setwd("C:/Users/ellen/OneDrive/MyDocs/Pycharm Projects/NHPN")
- 
+setwd("C:/Users/elco2649/Documents/NHPN/")
+
 #Read in roads:
-roads<- shapefile("C:/Users/ellen/OneDrive/MyDocs/Pycharm Projects/NHPN/Highways/NHPNLine.shp")
+roads<- shapefile("C:/Users/elco2649/Documents/NHPN/NHPNLine.shp")
 # roads<- shapefile("C:/Users/ellen/OneDrive/MyDocs/Pycharm Projects/NHPN/qgis_output/Collector_roads.shp")
 
-Roads<- spTransform(roads, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs "))
+# Roads<- spTransform(roads, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs "))
+Roads<- roads
 
 #Select Western and border states:
 Roads<- Roads[Roads$STATE_CODE %in% c(4, 6, 8, 16, 20, 30, 31, 32, 35, 38, 40, 41, 46, 48, 49, 53, 56),]
@@ -20,23 +21,22 @@ C_roads<- Roads[Roads$FCLASS %in% c(7, 8, 17),]
 
 
 #Read in monitors:
-all_monts<- read.csv("C:/Users/ellen/OneDrive/MyDocs/Pycharm Projects/NHPN/Locations_Dates_of_PM25_Obs.csv")
-mont_csv<- unique(all_monts[,c("Latitude", "Longitude")])
+all_monts<- read.csv("C:/Users/elco2649/Documents/NHPN/Projected_locations_part_a.csv")
+mont_csv<- unique(all_monts[,c("Lat", "Lon")])
 #mont_csv<- read.csv("Unique_monitors.csv")
-pos<- which((mont_csv[, "Latitude"] <= 50) & (mont_csv[, "Latitude"] >= 25) & 
-              (mont_csv[, "Longitude"] <= -101) & (mont_csv[, "Longitude"] >= -126)) #bounding box
+pos<- which((mont_csv[, "Lat"] <= 50) & (mont_csv[, "Lat"] >= 25) & 
+              (mont_csv[, "Lon"] <= -101) & (mont_csv[, "Lon"] >= -126)) #bounding box
 mont_csv<- mont_csv[pos,]
 mont_geom<- mont_csv
-coordinates(mont_geom)<- c("Longitude", "Latitude")
+coordinates(mont_geom)<- c("Lon", "Lat")
 proj4string(mont_geom)<- CRS("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
-                       
+
 monitors<- SpatialPointsDataFrame(mont_geom, mont_csv)
-Monitors<- spTransform(monitors, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs "))
- 
+Monitors<- spTransform(monitors, CRS( proj4string(A_roads)))
 
 #Make buffers 
-  #Note: this projection is in meters
-Buffers<- c(100, 250, 500, 10000)
+#Note: this projection is in meters
+Buffers<- c(100, 250, 500, 1000)
 
 for(b in 1:length(Buffers)){
   gbuffer<- gBuffer(Monitors, width = Buffers[b], byid= TRUE)
@@ -44,10 +44,10 @@ for(b in 1:length(Buffers)){
   #ARTERIAL
   #Sum road lengths which intersect
   AspInt<- over(gbuffer, A_roads[,c("MILES", "KM")], fn= sum)
-    #check: sum(!is.na(AspInt[,1]))
-
+  #check: sum(!is.na(AspInt[,1]))
+  
   Aoutput<- cbind(mont_csv, AspInt)
-  Afilename <- paste("C:/Users/ellen/OneDrive/MyDocs/Pycharm Projects/NHPN/R_output/Arterial_road_sum_", Buffers[b], ".csv", sep = "")
+  Afilename <- paste("C:/Users/elco2649/Documents/NHPN/R_output/Arterial_road_sum_", Buffers[b], ".csv", sep = "")
   write.csv(Aoutput, Afilename)
   
   #COLLECTOR
@@ -56,20 +56,14 @@ for(b in 1:length(Buffers)){
   #check: sum(!is.na(CspInt[,1]))
   
   Coutput<- cbind(mont_csv, CspInt)
-  Cfilename<- paste0("C:/Users/ellen/OneDrive/MyDocs/Pycharm Projects/NHPN/R_output/Collector_road_sum_", Buffers[b], ".csv", sep = "")
+  Cfilename<- paste0("C:/Users/elco2649/Documents/NHPN/R_output/Collector_road_sum_", Buffers[b], ".csv", sep = "")
   write.csv(Coutput, Cfilename)
   
   #BOTH types of roads: 
   Broads<- Reduce(function(x,y){x[is.na(x)] <- 0; y[is.na(y)] <- 0; x + y}, list(AspInt, CspInt))
   Boutput<- cbind(mont_csv, Broads)
-  Bfilename<- paste0("C:/Users/ellen/OneDrive/MyDocs/Pycharm Projects/NHPN/R_output/Both_types_road_sum_", Buffers[b], ".csv", sep = "")
+  Bfilename<- paste0("C:/Users/elco2649/Documents/NHPN/R_output/Both_types_road_sum_", Buffers[b], ".csv", sep = "")
   write.csv(Boutput, Bfilename)
   
   print(paste0("Buffer = ", Buffers[b], sep = ""))
 }
-
-
-
-
-
-
