4cdd69352a6aab3973956819d7ec6a32003cc797
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2018-10-23 11:09:06 -0600

    Checking for empty arrays

--- download-earth-observations/MAIAC_AOD/k_nearest_neighbors.py
+++ download-earth-observations/MAIAC_AOD/k_nearest_neighbors.py
@@ -5,12 +5,15 @@
 from sklearn.neighbors import NearestNeighbors
 import pandas as pd
 import numpy as np
-from csv import writer
+import csv
 import glob
-import os
+import os, shutil
+import datetime
 
 # date conversion code from: https://www.science-emergence.com/Articles/MODIS-Convert-Julian-Date-to-MMDDYYYY-Date-format-using-python/
 import calendar
+
+start = datetime.datetime.now()
 
 def JulianDate_to_MMDDYYY(y,jd):
     month = 1
@@ -30,61 +33,72 @@ def JulianDate_to_MMDDYYY(y,jd):
 def square(list):
     return [i ** 2 for i in list]
 
-# These are the monitoring locations:
-M_CSV = "/home/jovyan/MAIAC-AOD/Locations_Dates_of_PM25_Obs_DeDuplicate.csv" # 'C:/Users/elco2649/Documents/MAIAC/Locations_Dates_of_PM25_Obs_DeDuplicate.csv'
-monitors = pd.read_csv(M_CSV)
-monitors['Date'].astype('str')
+def main():
+    # These are the monitoring locations:
+    M_CSV = 'C:/Users/elco2649/Documents/MAIAC/Projected_locations_with_dates_part_a.csv' # '/home/jovyan/Projected_locations_with_dates_part_a.csv'
+    monitors = pd.read_csv(M_CSV)
+    monitors['Date'].astype('str')
 
-origpath = "/home/jovyan/MAIAC-AOD/avg_csv/" # 'C:/Users/elco2649/Documents/MAIAC/output_csvs/'
-outpath = "/home/jovyan/MAIAC-AOD/extracted/" # 'C:/Users/elco2649/Documents/MAIAC/extracted/'
+    origpath = 'C:/Users/elco2649/Documents/MAIAC/avg_csvs/'
+    outpath = 'C:/Users/elco2649/Documents/MAIAC/used_csvs/'
 
-# This is the output file
-with open(outpath + "MAIAC_extracted.csv", 'w') as dst:
-    writer = writer(dst, lineterminator='\n')
-    writer.writerow(["Latitude", "Longitude", "Datum", "Date", "MAIAC_AOD"])
+    # This is the output file
+    with open("C:/Users/elco2649/Documents/MAIAC/MAIAC_extracted.csv", 'a+') as dst:
+        writer = csv.writer(dst, lineterminator='\n')
+        writer.writerow(["Lat", "Lon", "Date", "MAIAC_AOD"])
 
-    # Loop through the MAIAC data (each file contains average values for a given day)
-    for csvfile in sorted(glob.glob(origpath + "*.csv")):
-        basename = os.path.basename(csvfile)
-        print(basename)
-        # convert date to useful format
-        day_string = basename[:-4]
-        year = day_string[:4]
-        JD = day_string[4:7]
-        #print(year + ", " + JD)
-        date = JulianDate_to_MMDDYYY(int(year), int(JD))
-        print(date)
+        # Loop through the MAIAC data (each file contains average values for a given day)
+        for csvfile in sorted(glob.glob(origpath + "*.csv")):
+            basename = os.path.basename(csvfile)
+            print(basename)
+            # convert date to useful format
+            day_string = basename[:-4]
+            year = day_string[:4]
+            JD = day_string[4:7]
+            #print(year + ", " + JD)
+            date = JulianDate_to_MMDDYYY(int(year), int(JD))
+            print(date)
 
-        # get monitor locations for this day
-        day_monts = monitors.loc[monitors['Date'] == date,:]
-        mont_coords = day_monts.loc[:,['Latitude', 'Longitude']]
-        # print(mont_coords.shape)
-        numObs = mont_coords.shape[0]
+            # get monitor locations for this day
+            day_monts = monitors.loc[monitors['Date'] == date,:]
+            if(day_monts.shape[0] > 1):
+                mont_coords = day_monts.loc[:,['Lat', 'Lon']]
+                # print(mont_coords.shape)
+                numObs = mont_coords.shape[0]
 
-        # read in the maiac data for this day
-        data = pd.read_csv(csvfile)
-        data_coords = data.loc[:, ['lat', 'long']]
-        # print(data_coords.shape)
+                # read in the maiac data for this day
+                data = pd.read_csv(csvfile)
+                if(data.shape[0] > 1):
+                    data_coords = data.loc[:, ['lat', 'long']]
+                    # print(data_coords.shape)
 
-        #Nearest Neighbors Classification: pick k
-        k = 8 # this approximates Queen Contiguity?
-        nbrs = NearestNeighbors(n_neighbors = k, algorithm = 'kd_tree').fit(data_coords, mont_coords)
-        distances, indices = nbrs.kneighbors(mont_coords)
-        # print("Distances = ", distances)
-        # print("Indices = ", indices)
+                    #Nearest Neighbors Classification: pick k
+                    k = 1 # this approximates Queen Contiguity?
+                    nbrs = NearestNeighbors(n_neighbors = k, algorithm = 'kd_tree').fit(data_coords, mont_coords)
+                    distances, indices = nbrs.kneighbors(mont_coords)
+                    # print("Distances = ", distances)
+                    # print("Indices = ", indices)
 
-        #collect and average the aod values of the k nearest neighbors
-        mont_aod = []
-        for i in range(0, numObs):
-            #print(csv[indices[i],3])
-            sq = np.array(square(distances[i]))
-            weights = 1. / sq #or figure out some other inverse-distance weighting system
-            #print(weights)
-            mont_aod.append(np.average(data.loc[indices[i],'aod'], weights = weights))
-        # print("AOD = ", mont_aod)
+                    #collect and average the aod values of the k nearest neighbors
+                    mont_aod = []
+                    for i in range(0, numObs):
+                        # #print(csv[indices[i],3])
+                        # sq = np.array(square(distances[i]))
+                        # weights = 1. / sq #or figure out some other inverse-distance weighting system
+                        # # print(weights)
+                        # mont_aod.append(np.average(data.loc[indices[i],'aod'], weights = weights))
+                        mont_aod.append((data.loc[indices[i],'aod']).values[0])
+                    # print("AOD = ", mont_aod)
+    
+                    for j in range(0, numObs):
+                        writer.writerow([mont_coords['Lat'].iloc[j], mont_coords['Lon'].iloc[j], date, mont_aod[j]])
 
-        for j in range(0, numObs):
-            writer.writerow([mont_coords['Latitude'].iloc[j], mont_coords['Longitude'].iloc[j], 'ESRI102003', date, mont_aod[j]])
+            shutil.move(csvfile, outpath + basename)
+
+    end = datetime.datetime.now()
+    print("Time for all files: ", end - start)
+    # Eventually, upload "MAIAC_extracted.csv" to S3
 
 
-# Eventually, upload "MAIAC_extracted.csv" to S3
+if __name__ == "__main__":
+    main()
