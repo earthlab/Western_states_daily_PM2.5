a166a9fcc0370f7f12dea993871073bb64e59956
Melissa Maestas <Melissa.Maestas@Colorado.edu>
2018-10-23 17:53:23 +0000

    working on county centroids

--- ML_estimations/ML_PM25_estimation_step2.R
+++ ML_estimations/ML_PM25_estimation_step2.R
@@ -24,6 +24,7 @@ this_datum <- "NAD83"
 ## set up documentation files/variables
 file_sub_label <- "CountyGeometricCentroids" # file partial name, decide whether to include date in file name
 title_string <- "Geometric Centroids of Counties" # used in figure titles, etc
+plot_name_extension <-  "MapLocations"
 LatexFileName=file.path(output.directory,paste("Rgenerated_",file_sub_label,"Images.tex",sep = "")) # Start file for latex code images
 LaTex_code_start_subsection.fn(LatexFileName, title_string, append_option = FALSE) # start subsection for latex code
 #SinkFileName=file.path(ProcessedData.directory,paste(file_sub_label,".txt",sep = "")) # file name
@@ -31,7 +32,7 @@ LaTex_code_start_subsection.fn(LatexFileName, title_string, append_option = FALS
 
 LaTex_code_start_subsection.fn(LatexFileName, title_string, append_option = FALSE) 
 
-FigFileName <- Plot_to_ImageFile_TopOnly.fn(output.directory, file_sub_label, plot_name_extension = "MapLocations") # start image file
+FigFileName <- Plot_to_ImageFile_TopOnly.fn(output.directory, file_sub_label, plot_name_extension = plot_name_extension) # start image file
 # create map of counties
 WestCountymapGeom <- map_county_base_layer.fn(CountyMaps.directory, study_states_abbrev)
 
@@ -47,6 +48,8 @@ county_centroids$Datum <- this_datum
 # plot centroids on map
 points(county_centroids$Longitude,county_centroids$Latitude,col="blue") # http://www.milanor.net/blog/maps-in-r-plotting-data-points-on-a-map/
 Plot_to_ImageFile_BottomOnly.fn(FigFileName = FigFileName, title_string = title_string) # finish image file
+
+LaTex_code_4_figure.fn(LatexFileName = LatexFileName, title_string = title_string, file_sub_label = file_sub_label, plot_name_extension = plot_name_extension, output.directory.short = output.directory.short)
 
 # write centroids to file
 write.csv(county_centroids,file = file.path(ProcessedData.directory,paste(file_sub_label,'_Locations_part_',processed_data_version,'.csv',sep = "")),row.names = FALSE)
