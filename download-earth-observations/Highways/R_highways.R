334f551c358a07efd9769eddab981404f88a4d9e
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2018-10-25 09:04:03 -0600

    After subsetting huge roads file

--- download-earth-observations/Highways/R_highways.R
+++ download-earth-observations/Highways/R_highways.R
@@ -5,20 +5,25 @@ library(rgeos)
 
 setwd("C:/Users/elco2649/Documents/NHPN/")
 
-#Read in roads:
-roads<- shapefile("C:/Users/elco2649/Documents/NHPN/NHPNLine.shp")
-# roads<- shapefile("C:/Users/ellen/OneDrive/MyDocs/Pycharm Projects/NHPN/qgis_output/Collector_roads.shp")
+# #Read in roads:
+# roads<- shapefile("C:/Users/elco2649/Documents/NHPN/NHPNLine.shp")
+# # roads<- shapefile("C:/Users/ellen/OneDrive/MyDocs/Pycharm Projects/NHPN/qgis_output/Collector_roads.shp")
+# 
+# #Note: we have to transform so the units are in meters
+# Roads<- spTransform(roads, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs "))
+# 
+# #Select Western and border states:
+# Roads<- Roads[Roads$STATE_CODE %in% c(4, 6, 8, 16, 20, 30, 31, 32, 35, 38, 40, 41, 46, 48, 49, 53, 56),]
+# 
+# #Select arterial and collector roads:
+# A_roads<- Roads[Roads$FCLASS %in% c(1, 2, 6, 11, 12, 14, 16),]
+# C_roads<- Roads[Roads$FCLASS %in% c(7, 8, 17),]
+# 
+# writeOGR(obj= A_roads, layer= "Arterial roads", "C:/Users/elco2649/Documents/NHPN/R_output/A_roads", driver="ESRI Shapefile")
+# writeOGR(obj= C_roads, layer= "Collector roads", "C:/Users/elco2649/Documents/NHPN/R_output/C_roads", driver="ESRI Shapefile")
 
-#Note: we have to transform so the units are in meters
-Roads<- spTransform(roads, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs "))
-
-#Select Western and border states:
-Roads<- Roads[Roads$STATE_CODE %in% c(4, 6, 8, 16, 20, 30, 31, 32, 35, 38, 40, 41, 46, 48, 49, 53, 56),]
-
-#Select arterial and collector roads:
-A_roads<- Roads[Roads$FCLASS %in% c(1, 2, 6, 11, 12, 14, 16),]
-C_roads<- Roads[Roads$FCLASS %in% c(7, 8, 17),]
-
+A_roads<- shapefile("C:/Users/elco2649/Documents/NHPN/R_output/A_roads.shp")
+C_roads<- shapefile("C:/Users/elco2649/Documents/NHPN/R_output/C_roads.shp")
 
 #Read in monitors:
 all_monts<- read.csv("C:/Users/elco2649/Documents/NHPN/Projected_locations_part_a.csv")
@@ -36,7 +41,7 @@ Monitors<- spTransform(monitors, CRS( proj4string(A_roads)))
 
 #Make buffers 
 #Note: this projection is in meters
-Buffers<- c(100, 250, 500, 1000)
+Buffers<- c(250, 500, 1000) #also 100
 
 for(b in 1:length(Buffers)){
   gbuffer<- gBuffer(Monitors, width = Buffers[b], byid= TRUE)
@@ -59,11 +64,12 @@ for(b in 1:length(Buffers)){
   Cfilename<- paste0("C:/Users/elco2649/Documents/NHPN/R_output/Collector_road_sum_", Buffers[b], ".csv", sep = "")
   write.csv(Coutput, Cfilename)
   
-  #BOTH types of roads: 
-  Broads<- Reduce(function(x,y){x[is.na(x)] <- 0; y[is.na(y)] <- 0; x + y}, list(AspInt, CspInt))
-  Boutput<- cbind(mont_csv, Broads)
-  Bfilename<- paste0("C:/Users/elco2649/Documents/NHPN/R_output/Both_types_road_sum_", Buffers[b], ".csv", sep = "")
-  write.csv(Boutput, Bfilename)
+  # #BOTH types of roads: 
+  # Broads<- Reduce(function(x,y){x[is.na(x)] <- 0; y[is.na(y)] <- 0; x + y}, list(AspInt, CspInt))
+  # Boutput<- cbind(mont_csv, Broads)
+  # Bfilename<- paste0("C:/Users/elco2649/Documents/NHPN/R_output/Both_types_road_sum_", Buffers[b], ".csv", sep = "")
+  # write.csv(Boutput, Bfilename)
   
   print(paste0("Buffer = ", Buffers[b], sep = ""))
 }
+
