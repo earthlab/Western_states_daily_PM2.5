ec21d7d5ad4859195c92089d0632fbe619525f96
Gina Li <gina.li@colorado.edu>
2018-10-30 17:01:50 -0600

    additional try/except

--- download-earth-observations/NED/extract_values_to_points.py
+++ download-earth-observations/NED/extract_values_to_points.py
@@ -70,8 +70,12 @@ if __name__ == "__main__":
                 time.sleep(5)
                 bbox_metadata = ulmo.usgs.ned.get_raster_availability('1 arc-second', bounding_boxes[i])
             except:
-                import IPython
-                IPython.embed()
+                try: 
+                    time.sleep(10)
+                    bbox_metadata = ulmo.usgs.ned.get_raster_availability('1 arc-second', bounding_boxes[i])
+                except:
+                    import IPython
+                    IPython.embed()
         #bbox_metadata = get_raster_availability_retry()
         tilename = bbox_metadata['features'][0]['properties']['download url'].split('/')[-1].split('.')[-2]+'.img'
         # next line not necessary
