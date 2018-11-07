a01a2f5da59340c3b6b69e8d74d619b1a82e14d2
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2018-11-06 20:22:01 -0700

    Done!

--- ML_estimations/PM25_county_plots.R
+++ ML_estimations/PM25_county_plots.R
@@ -1,12 +1,6 @@
-preds<- read.csv("C:/Users/ellen/OneDrive/MyDocs/Earth Lab Internship/Presentations/ML_Predictions_CountieswPredictors.csv")
-
-pred_data<- preds[which(preds$Date == "2008-07-11"),]
-
 library(geosphere)
-library(gtools)
 library(RColorBrewer)
-
-colors<- brewer.pal(n = 8, "YlOrRd")
+library(classInt)
 
 StateAbbrev2StateCode.fn <- function(StateAbbrev_vec) {
   #StateAbbrev_vec <- study_states_abbrev # example input
@@ -62,14 +56,7 @@ StateAbbrev2StateCode.fn <- function(StateAbbrev_vec) {
 } # end of StateAbbrev2StateCode.fn function
 
 map_county_base_layer.fn <- function(CountyMaps.directory, study_states_abbrev, PM25) {
-  
-  # Resources for mapping
-  # http://eriqande.github.io/rep-res-web/lectures/making-maps-with-R.html
-  
-  # Source for shapefiles:
-  # https://www.census.gov/geo/maps-data/data/tiger-cart-boundary.html 
-  # https://www.census.gov/geo/maps-data/data/cbf/cbf_counties.html
-  
+
   # create map
   Countymap=readOGR(dsn=file.path(CountyMaps.directory),layer = "cb_2017_us_county_500k") # load county map shapefile
   Countymap$STATEFP_NUM <- as.numeric(as.character(Countymap$STATEFP)) # have R recognize state FP's as numerical values (in a new column)
@@ -84,16 +71,41 @@ map_county_base_layer.fn <- function(CountyMaps.directory, study_states_abbrev, 
   WestCountymapGeom=Countymap[Countymap$STATEFP_NUM==4|Countymap$STATEFP_NUM==6|Countymap$STATEFP_NUM==8|Countymap$STATEFP_NUM==16|Countymap$STATEFP_NUM==30|Countymap$STATEFP_NUM==32|Countymap$STATEFP_NUM==35|Countymap$STATEFP_NUM==41|Countymap$STATEFP_NUM==49|Countymap$STATEFP_NUM==53|Countymap$STATEFP_NUM==56,]
   #print(WestUSmap)
   
-  q<- quantcut(PM25, q = 8)
-  q_colors<- colors[as.integer(q)]
+  plotvar <- PM25
+  class <- classIntervals(plotvar,
+                          nclr,
+                          style = "fixed",
+                          fixedBreaks = breaks)
+  colcode <- findColours(class, plotclr)
   
-  plot(WestCountymapGeom, col = q_colors)
-  legend(-101, 50, legend = levels(q), col = colors)
-  
-  return(WestCountymapGeom)
+  plot(WestCountymapGeom, col = colcode)
+  legend("bottomleft", # position
+         legend = names(attr(colcode, "table")), 
+         title = "Quantiles",
+         fill = attr(colcode, "palette"),
+         cex = 0.75,
+         bty = "n")
 }
 
 
 CountyMaps.directory = "C:/Users/ellen/OneDrive/MyDocs/Earth Lab Internship/Presentations/CountyMap"
 study_states_abbrev <- c("AZ","CA","CO", "ID", "MT", "NV", "NM", "OR", "UT", "WA", "WY")
-WestCountymapGeom <- map_county_base_layer.fn(CountyMaps.directory, study_states_abbrev, pred_data$PM25_prediction)
+
+preds<- read.csv("C:/Users/ellen/OneDrive/MyDocs/Earth Lab Internship/Presentations/ML_Predictions_CountieswPredictors.csv")
+pred_data<- preds[which(preds$Date == "2008-07-11"),]
+
+#Plotting help: https://www.r-bloggers.com/custom-legend-in-r/ 
+nclr<- 8
+plotclr <- brewer.pal(nclr, "YlOrRd")
+# breaks <- round(quantile(base_data$PM2.5_Obs, seq(0, 1, 1/nclr)),4)
+breaks <- round(quantile(pred_data$PM25_prediction, seq(0, 1, 1/nclr)), 4)
+
+map_county_base_layer.fn(CountyMaps.directory, study_states_abbrev, pred_data$PM25_prediction)
+
+base<- read.csv("C:/Users/ellen/OneDrive/MyDocs/Earth Lab Internship/Presentations/ML_input_PM25_Step5_part_d_de_duplicated_aves_ML_input.csv") 
+base_data<- base[which(base$Date == "2008-07-11"),]
+map_county_base_layer.fn(CountyMaps.directory, study_states_abbrev, base_data$PM2.5_Obs)
+
+############ MAIAC AOD
+breaks<- round(quantile(base_data$MAIAC_AOD, seq(0, 1, 1/nclr), na.rm = TRUE), 4)
+map_county_base_layer.fn(CountyMaps.directory, study_states_abbrev, base_data$MAIAC_AOD)
