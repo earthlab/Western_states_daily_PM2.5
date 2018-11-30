2cbd856d1e0027b2c3c685abda51f449c32cbd53
Melissa Maestas <Melissa.Maestas@Colorado.edu>
2018-11-30 16:23:00 +0000

    running step 2 of NAM processing

--- NAM_processing/Process_NAM_data_step2_parallel.R
+++ NAM_processing/Process_NAM_data_step2_parallel.R
@@ -25,10 +25,10 @@ grb1to2_conversion_prep.fn()
 
 #### define constants ####
 #study_start_date <- as.Date("20120107",format="%Y%m%d") # first date in study period
-study_start_date <- as.Date("20080101",format="%Y%m%d") # first date in study period
+study_start_date <- as.Date("20090101",format="%Y%m%d") # first date in study period
 print(study_start_date)
 #study_stop_date  <- as.Date("20180830",format="%Y%m%d") # last date in study period
-study_stop_date  <- as.Date("20080131",format="%Y%m%d") # last date in study period
+study_stop_date  <- as.Date("20091231",format="%Y%m%d") # last date in study period
 print(study_stop_date)
 Date_vector <- seq(study_start_date,study_stop_date, by = "day") # vector of all dates for which meteo data will be extracted
 n_days <- length(Date_vector)
