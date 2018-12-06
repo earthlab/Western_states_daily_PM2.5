2c184428d3e88397c55d8b062bc58d8452ee6569
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2018-12-06 11:36:54 -0700

    Using KNN

--- ML_estimations/PM25_county_plots.R
+++ ML_estimations/PM25_county_plots.R
@@ -1,84 +1,33 @@
-library(geosphere)
+
 library(RColorBrewer)
 library(classInt)
+library(sp)
+library(FNN)
+library(scales)
 
-StateAbbrev2StateCode.fn <- function(StateAbbrev_vec) {
-  #StateAbbrev_vec <- study_states_abbrev # example input
-  all_state_abbrevs_in <- unique(StateAbbrev_vec)
-  #all_state_abbrevs_out <- all_state_abbrevs_in
-  StateNum_vec <- StateAbbrev_vec
-  StateNum_vec[] <- NA
-  for (state_abbrev in all_state_abbrevs_in) {
-    print(state_abbrev)
-    if (state_abbrev == "AZ") {
-      state_number <-  4
-    } else if (state_abbrev == "CA") {
-      state_number <-  6
-    } else if (state_abbrev == "CO") {
-      state_number <-  8
-    } else if (state_abbrev == "ID") {
-      state_number <-  16
-    } else if (state_abbrev == "KS") {
-      state_number <-  20
-    } else if (state_abbrev == "MT") {
-      state_number <-  30
-    } else if (state_abbrev == "NE") {
-      state_number <-  31
-    } else if (state_abbrev == "NV") {
-      state_number <-  32
-    } else if (state_abbrev == "NM") {
-      state_number <-  35
-    } else if (state_abbrev == "ND") {
-      state_number <-  38
-    } else if (state_abbrev == "OK") {
-      state_number <-  40
-    } else if (state_abbrev == "OR") {
-      state_number <-  41
-    } else if (state_abbrev == "SD") {
-      state_number <-  46
-    } else if (state_abbrev == "TX") {
-      state_number <-  48
-    } else if (state_abbrev == "UT") {
-      state_number <-  49
-    } else if (state_abbrev == "WA") {
-      state_number <-  53
-    } else if (state_abbrev == "WY") {
-      state_number <-  56
-    } else {
-      #state_abbrev <- "--"
-      state_number <- NA
-    } # if (state_abbrev == "AZ") {
-    print(state_number)
-    which_this_abbrev <- which(StateAbbrev_vec == state_abbrev)
-    StateNum_vec[which_this_abbrev] <- state_number
-  } # for (this_state_abbrev in all_state_abbrevs_in) {
-  return(StateNum_vec)
-} # end of StateAbbrev2StateCode.fn function
-
-map_county_base_layer.fn <- function(CountyMaps.directory, study_states_abbrev, PM25) {
-
-  # create map
-  Countymap=readOGR(dsn=file.path(CountyMaps.directory),layer = "cb_2017_us_county_500k") # load county map shapefile
-  Countymap$STATEFP_NUM <- as.numeric(as.character(Countymap$STATEFP)) # have R recognize state FP's as numerical values (in a new column)
+map_AQ.fn<- function(shp, data, K, nclr, plotclr, breaks){
+  query<- coordinates(shp)
+  coords<- data[, c("Longitude", "Latitude")]
+  knn<- get.knnx(coords, query, k = K)
+  index<- knn$nn.index
+  dist<- knn$nn.dist
+  mat<- matrix(data[t(index), "AQ"], nrow = nrow(query), byrow = TRUE)
+  invdist<- t(apply(dist, 1, function(x)(x/sum(x))))
   
-  # this map is in NAD83, which can be verified with this command:
-  summary(Countymap) # summarize data
+  aq<- rep(0,nrow(query))
+  for (a in 1:length(aq)){
+    aq[a]<- sum(invdist[a,]*mat[a,])
+  }
   
-  State_Num_vec <- StateAbbrev2StateCode.fn(StateAbbrev_vec = study_states_abbrev)
-  #   # display the State FP values and state abbreviations next to each other
-  #   
-  #   # find the 11 western states included in the study
-  WestCountymapGeom=Countymap[Countymap$STATEFP_NUM==4|Countymap$STATEFP_NUM==6|Countymap$STATEFP_NUM==8|Countymap$STATEFP_NUM==16|Countymap$STATEFP_NUM==30|Countymap$STATEFP_NUM==32|Countymap$STATEFP_NUM==35|Countymap$STATEFP_NUM==41|Countymap$STATEFP_NUM==49|Countymap$STATEFP_NUM==53|Countymap$STATEFP_NUM==56,]
-  #print(WestUSmap)
-  
-  plotvar <- PM25
+  #Plotting help: https://www.r-bloggers.com/custom-legend-in-r/ 
+  plotvar <- aq
   class <- classIntervals(plotvar,
                           nclr,
                           style = "fixed",
                           fixedBreaks = breaks)
   colcode <- findColours(class, plotclr)
   
-  plot(WestCountymapGeom, col = colcode)
+  plot(shp, col = colcode)
   legend("bottomleft", # position
          legend = names(attr(colcode, "table")), 
          title = "Quantiles",
@@ -87,25 +36,30 @@ map_county_base_layer.fn <- function(CountyMaps.directory, study_states_abbrev, 
          bty = "n")
 }
 
+#Create Map
 
-CountyMaps.directory = "C:/Users/ellen/OneDrive/MyDocs/Earth Lab Internship/Presentations/CountyMap"
-study_states_abbrev <- c("AZ","CA","CO", "ID", "MT", "NV", "NM", "OR", "UT", "WA", "WY")
+CountyMaps.directory<- "C:/Users/ellen/OneDrive/MyDocs/Earth Lab Internship/Presentations/CountyMap"
+Countymap<- readOGR(dsn=file.path(CountyMaps.directory),layer = "cb_2017_us_county_500k") # load county map shapefile
+Countymap$STATEFP_NUM <- as.numeric(as.character(Countymap$STATEFP)) # have R recognize state FP's as numerical values (in a new column)
+# find the 11 western states included in the study
+WestCountymapGeom=Countymap[Countymap$STATEFP_NUM==4|Countymap$STATEFP_NUM==6|Countymap$STATEFP_NUM==8|Countymap$STATEFP_NUM==16|Countymap$STATEFP_NUM==30|Countymap$STATEFP_NUM==32|Countymap$STATEFP_NUM==35|Countymap$STATEFP_NUM==41|Countymap$STATEFP_NUM==49|Countymap$STATEFP_NUM==53|Countymap$STATEFP_NUM==56,]
 
-preds<- read.csv("C:/Users/ellen/OneDrive/MyDocs/Earth Lab Internship/Presentations/ML_Predictions_CountieswPredictors.csv")
-pred_data<- preds[which(preds$Date == "2008-07-11"),]
-
-#Plotting help: https://www.r-bloggers.com/custom-legend-in-r/ 
-nclr<- 8
-plotclr <- brewer.pal(nclr, "YlOrRd")
-# breaks <- round(quantile(base_data$PM2.5_Obs, seq(0, 1, 1/nclr)),4)
-breaks <- round(quantile(pred_data$PM25_prediction, seq(0, 1, 1/nclr)), 4)
-
-map_county_base_layer.fn(CountyMaps.directory, study_states_abbrev, pred_data$PM25_prediction)
+#Read in and format data 
+#Data table needs the columns "Longitude", "Latitude", and "AQ" (air quality measurement) 
 
 base<- read.csv("C:/Users/ellen/OneDrive/MyDocs/Earth Lab Internship/Presentations/ML_input_PM25_Step5_part_d_de_duplicated_aves_ML_input.csv") 
-base_data<- base[which(base$Date == "2008-07-11"),]
-map_county_base_layer.fn(CountyMaps.directory, study_states_abbrev, base_data$PM2.5_Obs)
+preds<- read.csv("C:/Users/ellen/OneDrive/MyDocs/Earth Lab Internship/Presentations/ML_Predictions_CountieswPredictors.csv")
 
-############ MAIAC AOD
-breaks<- round(quantile(base_data$MAIAC_AOD, seq(0, 1, 1/nclr), na.rm = TRUE), 4)
-map_county_base_layer.fn(CountyMaps.directory, study_states_abbrev, base_data$MAIAC_AOD)
+nclr<- 8
+base_data<- base[which(base$Date == "2008-07-11"), c("Longitude", "Latitude", "PM2.5_Obs")]
+colnames(base_data)<- c("Longitude", "Latitude", "AQ")
+base_breaks<- round(quantile(base_data$AQ, seq(0, 1, 1/nclr)), 4)
+pred_data<- preds[which(preds$Date == "2008-07-11"), c("Longitude", "Latitude", "PM25_prediction")]
+colnames(pred_data)<- c("Longitude", "Latitude", "AQ")
+pred_breaks<- round(quantile(pred_data$AQ, seq(0, 1, 1/nclr)), 4)
+
+#Plot actual PM2.5
+map_AQ.fn(WestCountymapGeom, base_data, K = 4, nclr, plotclr= brewer.pal(nclr, "YlOrRd"), base_breaks)
+
+#Plot predicted PM2.5
+map_AQ.fn(WestCountymapGeom, pred_data, K = 4, nclr, plotclr= brewer.pal(nclr, "YlOrRd"), pred_breaks)
