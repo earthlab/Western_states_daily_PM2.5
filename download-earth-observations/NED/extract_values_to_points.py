6fa1754c16a2c0bedafe1754560da2b7dbfb81d4
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2019-03-22 11:36:56 -0600

    Comment out Ipython.embeds

--- download-earth-observations/NED/extract_values_to_points.py
+++ download-earth-observations/NED/extract_values_to_points.py
@@ -145,13 +145,13 @@ if __name__ == "__main__":
                         except:
                             print("Tile extraction issue for tile " + tilename + " at lat/long " + str(station_locations[i]))
                             import IPython
-                            IPython.embed()
+#                             IPython.embed()
                             #raise ValueError('No value sampled')
         if len(elevation_values) != i + 1:
             print("elevation values do not match index, most likely failed to add an elevation value for a station location")
             raise ValueError("Elevation values do not match index, most likely failed to add an elecation value for a station location")
             import IPython
-            IPython.embed()
+#             IPython.embed()
     
     elevation_values = np.asarray(elevation_values)
 
