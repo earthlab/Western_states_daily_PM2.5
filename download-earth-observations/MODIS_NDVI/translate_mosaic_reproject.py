ef74d79d959ba2b607bfe07141e8858e5a0c6674
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2019-04-03 11:58:29 -0600

    Debugging

--- download-earth-observations/MODIS_NDVI/translate_mosaic_reproject.py
+++ download-earth-observations/MODIS_NDVI/translate_mosaic_reproject.py
@@ -26,9 +26,11 @@ if __name__ == "__main__":
         dataset = gdal.Open(filename)
         # get the first subdataset, which is the 1km monthly NDVI SDS
         ndvi_SDS = dataset.GetSubDatasets()[0][0]
+        print(ndvi_SDS)
         ndvi_tif = ".".join(ndvi_SDS.split('"')[1].split(".")[:-1]) + ".tif.tif"
         # translate it from HDF to TIF
         gdal_translate_str = 'gdal_translate -of GTiff "' + ndvi_SDS + '" ' + ndvi_tif
+        print(gdal_translate_str)
         os.system(gdal_translate_str)
     
     
