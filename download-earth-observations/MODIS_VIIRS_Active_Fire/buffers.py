8447f27ad6e4a61956835d54cf3500a16ba03e74
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2019-04-05 13:54:29 -0600

    Update buffers.py

--- download-earth-observations/MODIS_VIIRS_Active_Fire/buffers.py
+++ download-earth-observations/MODIS_VIIRS_Active_Fire/buffers.py
@@ -27,8 +27,7 @@ if __name__ == "__main__":
     idx = range(0, len(buffer_csv))
     buffer_csv['idx'] = idx
     buffer_gdf = gpd.read_file(args.buffer_shp)
-    Pos = pd.read_csv(args.matching_csv)
-    pos = Pos.loc[:,1]
+    pos = pd.read_csv(args.matching_csv)
     buffer_gdf = buffer_gdf.iloc[pos,]
     buffer_gdf['idx'] = idx
     
