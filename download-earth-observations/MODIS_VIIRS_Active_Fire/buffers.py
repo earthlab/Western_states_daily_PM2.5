34a0a63018f84ff2de7087b5855116d83dc413d4
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2019-04-05 14:22:11 -0600

    Update buffers.py

--- download-earth-observations/MODIS_VIIRS_Active_Fire/buffers.py
+++ download-earth-observations/MODIS_VIIRS_Active_Fire/buffers.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         fire_pts = fire_gdf[fire_gdf.geometry.intersects(buf.geometry)]
 
          # do a list intersection to find all shared dates
-        date_list = buffer_csv['dates'][index].split(',')
+        date_list = buffer_csv['Date'][index].split(',')
         datetimes = [datetime.strptime(d, '%Y-%m-%d') for d in date_list]
         buffer_dates = [datetime.strftime(dt, '%m/%d/%Y') for dt in datetimes]
         fire_dates = fire_pts['adj_date'].values
