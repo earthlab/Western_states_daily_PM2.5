a870cde3efb1ca2d336bfe2e32cdd600e627f1ee
Melissa May Maestas <Melissa.Maestas@Colorado.edu>
2019-04-10 19:57:25 +0000

    comment/clean code for NAM step 3

--- NAM_processing/Process_NAM_data_step3.R
+++ NAM_processing/Process_NAM_data_step3.R
@@ -1,7 +1,7 @@
 # Process_NAM_data_step3.R - merge all of the extracted NAM data files into a single file
 
 #### Clear variables and sinks; define working directory ####
-rm(list  =  ls())
+rm(list  =  ls()) # clear all variables
 options(warn  =  2) # throw an error when there's a warning and stop the code from running further
 if (max(dev.cur())>1) { # make sure it isn't outputting to any figure files
   dev.off(which  =  dev.cur())
@@ -9,58 +9,52 @@ if (max(dev.cur())>1) { # make sure it isn't outputting to any figure files
 while (sink.number()>0) {
   sink()
 } # while (sink.number()>0) {
-working.directory  <-  "/home/rstudio"
+working.directory  <-  "/home/rstudio" # define working directory
 setwd(working.directory) # set working directory
 
 #### Source functions I've written ####
-source(file.path("estimate-pm25","General_Project_Functions","general_project_functions.R"))
+source(file.path("estimate-pm25","General_Project_Functions","general_project_functions.R")) # used for defining file paths
 
 #### Define constants/folders ####
-#NAM_processed_data_version <- "bc" # data part
-NAM_folder <- "NAM_data"
-input_sub_folder <- "NAM_Step2"
-output_sub_folder <- "NAM_Step3"
-intermediary_sub_folder <- "NAM_Step3_Intermediary_Files"
-output_file_name <- paste("NAM_Step3_processed_",Sys.Date(),sep = "")
-#sub_folder <- paste("NAM_data_part_",NAM_processed_data_version,sep = "") # subfolder withing ProcessedData.directory
-#output_file_name <- "NAM_Step3_part_bc"
+NAM_folder <- "NAM_data" # define folder for NAM data
+input_sub_folder <- "NAM_Step2" # define location of input files
+output_sub_folder <- "NAM_Step3" # define location for output files
+intermediary_sub_folder <- "NAM_Step3_Intermediary_Files" # define location for intermediary files (within NAM_Step3)
+output_file_name <- paste("NAM_Step3_processed_",Sys.Date(),sep = "") # define name of output file
 
 #### Load and process data ####
-#file_name_pattern <- "UTC\\.csv$"
-#this_file_list_step <- list.files(path = file.path(define_file_paths.fn("ProcessedData.directory"),sub_folder), pattern = file_name_pattern)
+# get list of all NAM Step 2 file names:
 this_file_list_step <- list.files(path = file.path(define_file_paths.fn("ProcessedData.directory"),NAM_folder,input_sub_folder,"."), pattern = NULL, all.files = FALSE,
                           full.names = FALSE, recursive = FALSE,
                           ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
-
-print(paste("There are ",length(this_file_list_step),"files for NAM (extracted to points)"))    
+print(paste("There are ",length(this_file_list_step),"files for NAM (extracted to points)")) # optional output statement
 
 # find out how many files only have 6 columns (didn't have weather data) by figuring out how many columns each file has
-n_cols <- NA
-n_cols <- unlist(lapply(1:length(this_file_list_step), function(file_i){
-  file_name <- this_file_list_step[file_i]
-  #this_data <- read.csv(file.path(define_file_paths.fn("ProcessedData.directory"),sub_folder,file_name))
-  this_data <- read.csv(file.path(define_file_paths.fn("ProcessedData.directory"),NAM_folder,input_sub_folder,file_name))
-  this_n_cols <- dim(this_data)[2]
-  rm(this_data)
-  return(this_n_cols)
+n_cols <- NA # re-allocate
+n_cols <- unlist(lapply(1:length(this_file_list_step), function(file_i){ # start definition of anonymous function
+  file_name <- this_file_list_step[file_i] # get file name
+  this_data <- read.csv(file.path(define_file_paths.fn("ProcessedData.directory"),NAM_folder,input_sub_folder,file_name)) # load Step 2 file
+  this_n_cols <- dim(this_data)[2] # determine the number of columns
+  rm(this_data) # clear variable
+  return(this_n_cols) # output from function
 })) # n_cols <- unlist(lapply(1:length(this_file_list_step), function(file_i){
 
 which_odd_files <- which(n_cols != median(n_cols)) # bad files have 6 columns (location & date information but no weather data)
-print(paste("There are ",length(which_odd_files),"files that do not have have",median(n_cols),"columns and will be disregarded (no weather data got put in):"))
-odd_file_names <- this_file_list_step[which_odd_files]
-for (file_i in 1:length(odd_file_names)) {
-  file_name <- odd_file_names[file_i]
-  print(file_name)
-  #this_data <- read.csv(file.path(ProcessedData.directory,sub_folder,file_name))
-}
+print(paste("There are ",length(which_odd_files),"files that do not have have",median(n_cols),"columns and will be disregarded (no weather data got put in):")) # optional output statement
+odd_file_names <- this_file_list_step[which_odd_files] # isolate names of bad files
+print(odd_file_names) # output names of bad files to screen
+#for (file_i in 1:length(odd_file_names)) { # cycle through each bad file name
+#  file_name <- odd_file_names[file_i] 
+#  print(file_name)
+#}
 
 # process the remaining good files
-which_good_files <- which(n_cols == median(n_cols))
+which_good_files <- which(n_cols == median(n_cols)) # determine which files are likely to be good (they have enough columns)
 print(paste("There are ",length(which_good_files)," files that have 19 columns (the expected number) and these will be processed."))
 this_file_list <- this_file_list_step[which_good_files] # list of only good files
 
 # the files have the time stamp just in the file name, so the time stamp needs to be added as a column in the data
-new_file_list <- unlist(lapply(this_file_list[1:2], function(x){ # start lapply and start defining function used in lapply
+new_file_list <- unlist(lapply(this_file_list, function(x){ # start lapply and start defining function used in lapply
   this_UTC_timestamp <- substr(x,nchar(x)-24,nchar(x)-23) # identify the time stamp for the file in this iteration
   #print(this_UTC_timestamp)
   this_data <- read.csv(file.path(define_file_paths.fn("ProcessedData.directory"),NAM_folder,input_sub_folder,x)) # open data file
@@ -74,8 +68,5 @@ new_file_list <- unlist(lapply(this_file_list[1:2], function(x){ # start lapply 
       
 setwd(file.path(define_file_paths.fn("ProcessedData.directory"),NAM_folder,output_sub_folder,intermediary_sub_folder)) # change working directory so the next step will work
 Step3_NAM_data <- do.call(rbind,lapply(new_file_list, read.csv)) # open and bind all files in list together # https://stackoverflow.com/questions/23995384/read-and-rbind-multiple-csv-files  
-setwd(working.directory)
-# write data to file
-write.csv(Step3_NAM_data,file = file.path(define_file_paths.fn("ProcessedData.directory"),NAM_folder,output_sub_folder,paste(output_file_name,".csv",sep = "")),row.names = FALSE)
-
-
+setwd(working.directory) # go back to original working directory
+write.csv(Step3_NAM_data,file = file.path(define_file_paths.fn("ProcessedData.directory"),NAM_folder,output_sub_folder,paste(output_file_name,".csv",sep = "")),row.names = FALSE) # write data to file
