1e3f72e9f1d7dc81f685f2d33a59c961d54f21c8
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2019-05-23 14:32:08 -0600

    Incorporated parallelization

--- ML_estimations/Get_tuning_parameters.R
+++ ML_estimations/Get_tuning_parameters.R
@@ -2,6 +2,8 @@
 library(caret)
 library(caretEnsemble)
 library(dplyr) #for sample_n
+library(parallel)
+library(doParallel)
 
 #Read in the data
 Fire_2010<- read.csv("C:\\Users\\elco2649\\Documents\\Machine Learning\\ML_input_Fire_2010.csv") #55840, 67
@@ -13,12 +15,18 @@ Subsets<- list(Fire_2010, NotFire_2010, Fire_2017, NotFire_2017)
 
 i<- 1
 
-sink("C:\\Users\\elco2649\\Documents\\Machine Learning\\Get_tuning_parameters_and_compare_algs.txt")
+#Log results
+sink("C:\\Users\\elco2649\\Documents\\Machine Learning\\Get_tuning_parameters_and_compare_algs_PARALLEL.txt")
 
 for(SS in Subsets){
   print(paste("Subset =", i))
   i <- i+1
   for(seed in c(321, 654, 987)){
+    #Parallelization: https://topepo.github.io/caret/parallel-processing.html
+      #Note: I put the parallelization here so that the models aren't written to the sink file out-of-order
+    cluster <- makeCluster(detectCores() - 2)
+    registerDoParallel(cluster)
+    
     print(paste("Seed =", seed))
     #Take sample
     set.seed(seed)
@@ -30,7 +38,8 @@ for(SS in Subsets){
     train<- dataset[-test_pos,]
     #Set up control object
     myControl <- trainControl(method = "repeatedcv", number = 10, repeats = 3, search = "grid", 
-                              savePredictions = "final", index = createResample(train$PM2.5_Obs, 10))
+                              savePredictions = "final", index = createResample(train$PM2.5_Obs, 10),
+                              verboseIter = FALSE)
     #MACHINE LEARNING
     
     #Random Forest
@@ -42,7 +51,7 @@ for(SS in Subsets){
     print(ranger_model$results)
     print(ranger_model$bestTune)
     ranger_end<- Sys.time()
-    print(paste("ranger:", ranger_end - ranger_start))
+    print(paste("ranger:", ranger_end - ranger_start, "minutes"))
     
     
     other_algs<- c("gbm", "gaussprRadial", "xgbTree", "bagEarth", "glmnet")
@@ -53,12 +62,15 @@ for(SS in Subsets){
       print(model$results)
       print(model$bestTune)
       model_end<- Sys.time()
-      print(paste0(A, ": ", model_end - model_start))
+      print(paste0(A, ": ", model_end - model_start, " minutes"))
     }
     
+    stopCluster(cluster)
   }
 }
 
 sink()
+
+
 
 
