3ca1f7dc2645edae82304493fe77701968c70433
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2019-07-03 14:58:03 -0600

    Leave out negative values

--- PM25-estimation/Format_UT_data.R
+++ PM25-estimation/Format_UT_data.R
@@ -6,27 +6,27 @@ library(rgdal)
 stations<- read.csv("C:\\Users\\ellen\\OneDrive\\MyDocs\\Earth Lab Internship\\Spatial_Processing\\UT-PM2.5-stations.csv")
 
 extract_UT_data<- function(filename, year){
+  #Read in data
   S1<- names(read.csv(filename))[-1]
-  raw1<- read.csv(filename, skip = 2)
+  raw1<- as.matrix(read.csv(filename, skip = 2))
+  #Prepare for merging (later on)
   matched<- match(S1, stations$Name)
   
+  #Get data in useable format
   days<- sort(rep(1:365, 24)) #366 for 2016
-  daily<- cbind(unlist(apply(raw1[,-1], MARGIN = 2, function(y){
-    aggregate(x = y, by = list(days), FUN = mean)[,2] })))
+  station.ID<- as.vector(sapply(matched, function(y){rep(stations[y,1],365*24)})) #366 for 2016
+  all_data<- as.numeric(as.vector(raw1[,-1]))
+  ALL<- cbind(all_data, station.ID, days)
+  ALL<- ALL[-which(ALL[,1] < 0),]
+  daily<- aggregate(all_data ~ days + station.ID, data = ALL, FUN = mean)
+  daily$Daily<- sapply(daily$days, function(y){format(as.Date(y, origin = paste0(year-1,"-12-31")), "%Y-%m-%d")})
+  data<- daily[,c(2,4,3)]
+  names(data)<- c("Station", "Date", "PM2.5")
+
+  #Merge with station information
+  Data<- merge(data, stations, by.x = "Station", by.y = names(stations)[1])
   
-  Daily<- data.frame(Date = seq.Date(as.Date(paste0(year, "-1-1")), as.Date(paste0(year, "-12-31")), by = "day"), daily)
-  
-  data<- Daily[,1:2]
-  names(data)<- c("Date", "X")
-  for(j in 3:dim(Daily)[2]){
-    new<- Daily[,c(1,j)]
-    names(new)<- c("Date", "X")
-    data<- rbind(data, new)
-  }
-  
-  data$station.ID<- as.vector(sapply(matched, function(y){rep(stations[y,1],365)})) #366 for 2016
-  Data<- merge(data, stations, by.x = "station.ID", by.y = names(stations)[1])
-  
+  #Get lat,lon from UTM
   sputm <- SpatialPoints(Data[,c("UTM.Easting", "UTM.Northing")], proj4string=CRS("+proj=utm +zone=12S +datum=WGS84")) 
   spgeo <- spTransform(sputm, CRS("+proj=longlat +datum=WGS84"))
   Coords<- as.data.frame(spgeo@coords)
