81db3831e41845a9049f51ca6140b2d75f6eaa81
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2019-07-08 09:00:38 -0600

    Update Get_population_density.R

--- download-earth-observations/Get_population_density.R
+++ download-earth-observations/Get_population_density.R
@@ -3,12 +3,12 @@ library(raster)
 
 #Read in data
 shp<- shapefile("C:\\Users\\elco2649\\Documents\\Census_data\\US_2010_census_tracts_NAD83.shp") #FIPS have leading zeros
-MLdata<- read.csv("C:\\Users\\elco2649\\Documents\\Machine Learning\\ML_input_no-GASP_5-21-2019.csv")
+Loc_data<- read.csv("C:\\Users\\elco2649\\Documents\\PM25_Step3_part_f_Locations_NAD83.csv")
 PD<- read.csv("C:\\Users\\elco2649\\Documents\\Census_data\\Census_2010_pop_density.csv")
 # pd<- read.csv("C:\\Users\\elco2649\\Documents\\Census_data\\ACS-2011-2015_pop_density.csv")
 
 #Prepare points and polygons
-coords<- as.data.frame(MLdata[,c("Longitude", "Latitude")])
+coords<- as.data.frame(Loc_data[,c("Lon", "Lat")]) #c("Longitude", "Latitude") 
 pointsSP<- SpatialPoints(coords, proj4string=CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
 
 #Merge open source PD data with shp
@@ -23,9 +23,9 @@ fine<- SHP[-which(is.na(SHP$SE_T002_001)),]
 indices<- over(pointsSP, SHP)
 
 #Merge data
-DATA<- cbind(MLdata, Pop_density = indices[,"SE_T002_001"])
+DATA<- cbind(Loc_data, Pop_density = indices[,"SE_T002_002"])
 # DATA$Log_PM2.5_Obs<- log(DATA$PM2.5_Obs)
 DATA2<- DATA[-which(is.na(DATA$Pop_density)),] #The 3 NAs are from a location in Mexico
 
 #Write to file
-write.csv(DATA2, "C:\\Users\\elco2649\\Documents\\Machine Learning\\ML_input_7-2-2019.csv")
+write.csv(DATA2, "C:\\Users\\elco2649\\Documents\\Census_data\\Pop_density_part_f.csv")
