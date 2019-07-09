94929dd16a2f8ff7083c397f2904d52f3de0385c
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2019-07-09 11:34:29 -0600

    Update extract_to_values.py

--- download-earth-observations/MODIS_NDVI/extract_to_values.py
+++ download-earth-observations/MODIS_NDVI/extract_to_values.py
@@ -30,13 +30,13 @@ if __name__ == "__main__":
     # read csv file into pandas dataframe
     df = pd.read_csv(args.input_csv_file)
     
-    #Extra work for batch g
-    d = pd.DataFrame({'Date': pd.date_range("2008-01-01", "2018-12-31")})
-    D = pd.DataFrame(np.repeat(d.values,len(df),axis=0))
-    D.columns = d.columns
-    df_repeated = pd.concat([df]*len(d), ignore_index=True)
-    final_df = pd.concat([D, df_repeated], axis=1, ignore_index=True)
-    final_df.columns = D.columns.append(df.columns)
+#     #Extra work for batch g
+#     d = pd.DataFrame({'Date': pd.date_range("2008-01-01", "2018-12-31")})
+#     D = pd.DataFrame(np.repeat(d.values,len(df),axis=0))
+#     D.columns = d.columns
+#     df_repeated = pd.concat([df]*len(d), ignore_index=True)
+#     final_df = pd.concat([D, df_repeated], axis=1, ignore_index=True)
+#     final_df.columns = D.columns.append(df.columns)
 
     #Back to original code
     station_locations = []
@@ -44,27 +44,28 @@ if __name__ == "__main__":
     ndvi_values = []
 
     for index, row in final_df.iterrows(): #used to just be df
-        lon = round(row['Easting'], 6)
-        lat = round(row['Northing'], 6)
-        date_str = row['Date']
-        print(lon,lat)
-        print(date_str)
-        date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
-        julian_date_str = date_obj.strftime("%Y%j")
-        print(julian_date_str)
-        year = date_obj.year
-        if calendar.isleap(year):
-                month_start_list = [1, 32, 61, 92, 122, 153, 183, 214, 245, 275, 306, 336]
-        else:
-            month_start_list = [1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
-        
-        mosaic_file_julian_day = str(year) + str(month_start_list[date_obj.month-1]).zfill(3)
-        mosaic_file = args.NDVI_directory + '*' + mosaic_file_julian_day + '.mosaic.reproj.tif.tif'
+        for date_str in pd.date_range("2008-01-01", "2018-12-31"):
+            lon = round(row['Easting'], 6)
+            lat = round(row['Northing'], 6)
+    #         date_str = row['Date']
+            print(lon,lat)
+            print(date_str)
+            date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
+            julian_date_str = date_obj.strftime("%Y%j")
+            print(julian_date_str)
+            year = date_obj.year
+            if calendar.isleap(year):
+                    month_start_list = [1, 32, 61, 92, 122, 153, 183, 214, 245, 275, 306, 336]
+            else:
+                month_start_list = [1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
 
-        for fn in glob.glob(mosaic_file):
-            print("reading from this file: " + fn)
-            ndvi_values.append(get_NDVI_value_at_point(fn, (lon, lat)))
-            print("added this NDVI value from the file: " + str(get_NDVI_value_at_point(fn, (lon, lat))))
+            mosaic_file_julian_day = str(year) + str(month_start_list[date_obj.month-1]).zfill(3)
+            mosaic_file = args.NDVI_directory + '*' + mosaic_file_julian_day + '.mosaic.reproj.tif.tif'
+
+            for fn in glob.glob(mosaic_file):
+                print("reading from this file: " + fn)
+                ndvi_values.append(get_NDVI_value_at_point(fn, (lon, lat)))
+                print("added this NDVI value from the file: " + str(get_NDVI_value_at_point(fn, (lon, lat))))
 
     ndvi_values = np.asarray(ndvi_values)*0.0001
     ndvi_values.round(decimals=4)
