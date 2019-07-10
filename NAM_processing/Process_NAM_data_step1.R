65bcbe3b68f0af29dac9f6437fccd10a10814b78
Melissa May Maestas <Melissa.Maestas@Colorado.edu>
2019-07-10 15:28:29 +0000

    update NAM step1 processing

--- NAM_processing/Process_NAM_data_step1.R
+++ NAM_processing/Process_NAM_data_step1.R
@@ -1,7 +1,8 @@
-# Process NAM data
+# Process NAM data Step 1 - add next day to list of dates that need to be processed for NAM data
+# this is because NAM data is in UTC (England) time, and has to be converted to the local time zones
 
 #### Clear variables and sinks; define working directory ####
-rm(list  =  ls())
+rm(list  =  ls()) # clear all variables
 options(warn  =  2) # throw an error when there's a warning and stop the code from running further
 if (max(dev.cur())>1) { # make sure it isn't outputting to any figure files
   dev.off(which  =  dev.cur())
@@ -24,14 +25,18 @@ source(file.path(define_file_paths.fn("NAM_Code.directory"),"NAM_processing_func
 sub_folder <- "NAM_data" # define sub-folder name
 
 # Define file names and file paths for Date/Locations of interest # 
-locations_files <- c(paste("PM25_Step3_part_",define_study_constants.fn("processed_data_version"),"_Locations_Dates_NAD83", sep = ""),paste("CountyCentroid_Locations_Dates_", define_study_constants.fn("start_date"),"to",define_study_constants.fn("end_date"),sep = ""))
-locations_subfolders <- c("PM25_Locations_Dates","CountyCentroid")
+#locations_files <- c(paste("PM25_Step3_part_",define_study_constants.fn("processed_data_version"),"_Locations_Dates_NAD83", sep = ""),paste("CountyCentroid_Locations_Dates_", define_study_constants.fn("start_date"),"to",define_study_constants.fn("end_date"),sep = ""))
+#locations_subfolders <- c("PM25_Locations_Dates","CountyCentroid")
+
+# Define file names and file paths for Date/Locations of interest - not for data sets for which we are processing all dates in study period at a list of locations 
+locations_subfolders <- c(paste("PM25_data_part_",define_study_constants.fn("processed_data_version"),sep = ""))
+locations_files <- c(paste("PM25_Step3_part_",define_study_constants.fn("processed_data_version"),"_Locations_Dates_NAD83", sep = ""))#,paste("CountyCentroid_Locations_Dates_", define_study_constants.fn("start_date"),"to",define_study_constants.fn("end_date"),sep = ""))
 
 # lapply function to load data, add rows for all locations with next day, save to csv file
 lapply(1:length(locations_files), function(x) { # start lapply function
   this_location_date_file <- locations_files[x] # get file name
   print(this_location_date_file) # print to screen
-  locations_subfolder <- locations_subfolders[x]
+  locations_subfolder <- locations_subfolders[x] # get the name of the subfolder for this iteration
   PM25DateLoc_orig <-read.csv(file.path(define_file_paths.fn("ProcessedData.directory"),locations_subfolder,paste(this_location_date_file,".csv",sep = "")),header=TRUE) # load the locations file
   PM25DateLoc_orig$Date <- as.Date(PM25DateLoc_orig$Date) # recognize date column as dates
   PM25DateLoc_wNextDay <- add_next_day_date_loc.fn(PM25DateLoc_orig) # put in the day following each date 
@@ -43,5 +48,5 @@ lapply(1:length(locations_files), function(x) { # start lapply function
 }) # end lapply function
 
 #### End of file cleanup
-rm(working.directory,start_code_timer,sub_folder)
-rm(locations_files,locations_subfolders)
+rm(working.directory,start_code_timer,sub_folder) # clear variables
+rm(locations_files,locations_subfolders) # clear variables
