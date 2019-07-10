7cdec4e01c801e37501c35a01e9a4c47e531e728
EllenConsidine <35497805+EllenConsidine@users.noreply.github.com>
2019-07-10 09:48:57 -0600

    Including number of non-negative observations

--- PM25-estimation/Format_UT_data.R
+++ PM25-estimation/Format_UT_data.R
@@ -13,15 +13,22 @@ extract_UT_data<- function(filename, year){
   matched<- match(S1, stations$Name)
   
   #Get data in useable format
-  days<- sort(rep(1:365, 24)) #366 for 2016
-  station.ID<- as.vector(sapply(matched, function(y){rep(stations[y,1],365*24)})) #366 for 2016
+  days<- sort(rep(1:365, 24)) #365 for 2016
+  station.ID<- as.vector(sapply(matched, function(y){rep(stations[y,1],365*24)})) #365 for 2016
   all_data<- as.numeric(as.vector(raw1[,-1]))
   ALL<- cbind(all_data, station.ID, days)
   ALL<- ALL[-which(ALL[,1] < 0),]
+  #Calculate number of non-negative observations per 
+  Freq<- table(interaction(ALL[,3], ALL[,2]))
+  #Aggregate into 24-hour average
   daily<- aggregate(all_data ~ days + station.ID, data = ALL, FUN = mean)
+  Freq2<- interaction(daily$days, daily$station.ID)
+  pos<- which(as.character(names(Freq)) %in% as.character(Freq2))
+  Freq3<- Freq[pos]
   daily$Daily<- sapply(daily$days, function(y){format(as.Date(y, origin = paste0(year-1,"-12-31")), "%Y-%m-%d")})
   data<- daily[,c(2,4,3)]
   names(data)<- c("Station", "Date", "PM2.5")
+  data$Num_Obs<- Freq3
 
   #Merge with station information
   Data<- merge(data, stations, by.x = "Station", by.y = names(stations)[1])
@@ -42,5 +49,3 @@ extract_UT_data("C:\\Users\\ellen\\Downloads\\2015-PM2.5.csv", 2015)
 extract_UT_data("C:\\Users\\ellen\\Downloads\\2016-PM2.5.csv", 2016)
 extract_UT_data("C:\\Users\\ellen\\Downloads\\2017-PM2.5.csv", 2017)
 extract_UT_data("C:\\Users\\ellen\\Downloads\\2018-PM2.5.csv", 2018)
-
-
