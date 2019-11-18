65a5b8242e099b684e3a026124dc41e2324d88e0
Melissa May Maestas <Melissa.Maestas@Colorado.edu>
2019-11-18 18:48:56 +0000

    update caption for figure 1

--- LaTeX_documentation/SciData_manuscript/Figure_Table_Scripts/Choropleths_at_ZipCodes_4panel.R
+++ LaTeX_documentation/SciData_manuscript/Figure_Table_Scripts/Choropleths_at_ZipCodes_4panel.R
@@ -15,8 +15,43 @@ setwd(working.directory) # set working directory
 
 #### Load Functions that I created ####
 source(file.path("estimate-pm25","General_Project_Functions","general_project_functions.R"))
-source(file.path("estimate-pm25","General_Project_Functions","merging_data_functions.R"))
+source(file.path(define_file_paths.fn("ML_Code.directory"),"Plotting_and_LaTex_functions.R"))
+source(file.path(define_file_paths.fn("ML_Code.directory"),"Mapping_functions.R"))
+
+#### Define file path ####
+output.directory <- file.path("/home/rstudio","estimate-pm25","LaTeX_documentation","SciData_manuscript")
+
+#### Load data file ####
+full_df <- read.csv(file.path("/home","rstudio","Processed_Data","ML_input_files","ML_input_PM25_Step4_part_f_de_duplicated_aves.csv"),header=TRUE) # load the file
+
+#### Process the data ####
+# Set classes of columns
+full_df$Date <- as.Date(full_df$Date,"%Y-%m-%d") # recognize dates as dates: 'Date_Local' 
+df_step <- full_df[ , c("Date",col_name_interest,"Latitude","Longitude")]
+df_in <- na.rm(df_step)
+
+#### Define file names ####
+Top_latex_file_name <- file.path(define_file_paths.fn("output.directory"),"Report_top_template.tex")
+Main_latex_file_name <- file.path(output.directory,paste("Rgenerated_Fig2_choropleth_4-panel_main.tex",sep = ""))
+Bottom_latex_file_name <- file.path(define_file_paths.fn("output.directory"),"Report_bottom_template.tex")
+Report_latex_file_name <- file.path(output.directory,paste("Rgenerated_Fig2_choropleth_4-panel.tex",sep = ""))
+file_sub_label <- "Fig2_choropleth_4-panel"
+LatexFileName=file.path(define_file_paths.fn("output.directory"),paste("Rgenerated_",file_sub_label,"MapCountyMonthlyImages.tex",sep = "")) # Start file for latex code images
+
+#### Define Variables ####
+col_name_interest <- "PM2.5_Obs"
+study_states_abbrev = define_study_constants.fn("study_states_abbrev")
 
 ### Create Figure ####
+
+if (sink.number()>0) {sink()} # get stop any lingering sinks
+if (file.exists(LatexFileName) == TRUE) {file.remove(LatexFileName)}
+print("map county monthly median values- PM2.5 only")
+map_spec_days_value_by_region.fn(Region = "County", RegionMaps.directory = define_file_paths.fn("CountyMaps.directory"),
+                                 df_in = df_in, dates_of_interest = NA, Date_col = "Date",
+                                 Lat_col = "Latitude", Lon_col = "Longitude", Var_col = col_name_interest,
+                                 Cut_points_set = TRUE, cut_point_scale = col_name_interest, study_states_abbrev = study_states_abbrev,
+                                 output.directory = output.directory,file_sub_label = file_sub_label,
+                                 LatexFileName = LatexFileName,title_string_starter = title_string_starter, Time_aggregation = "Monthly")
 
 
--- LaTeX_documentation/SciData_manuscript/Figure_Table_Scripts/Map_Monitor_Locations.R
+++ LaTeX_documentation/SciData_manuscript/Figure_Table_Scripts/Map_Monitor_Locations.R
@@ -31,6 +31,7 @@ plot_name_extension = "Locations"
 study_states_abbrev = define_study_constants.fn("study_states_abbrev")
 this_datum <- define_study_constants.fn("study_datum")
 title_string <- "Observation Locations"
+caption_string <- "Locations of PM\\textsubscript{2.5} monitoring stations that had at least one observation during our study period (2008-2018)."
 ClearPage = FALSE
 LatexFileName <- file.path("/home/rstudio","estimate-pm25","LaTeX_documentation","SciData_manuscript","Fig1MonitorLocations.tex")
 if (exists(LatexFileName)) {
@@ -43,25 +44,8 @@ if (exists(LatexFileName)) {
   plot_color <- "blue"
   points(this_df$Longitude,this_df$Latitude,pch = 19,col=plot_color)#, 
   Plot_to_ImageFile_BottomOnly.fn(FigFileName = FigFileName, title_string = title_string) # finish image file
-  LaTex_code_4_figure.fn(LatexFileName = LatexFileName, title_string = title_string, file_sub_label = file_sub_label, plot_name_extension = plot_name_extension, output.directory.short = output.directory.short, image_format = "jpg", ClearPage = ClearPage)
-
+  LaTex_code_4_figure.fn(LatexFileName = LatexFileName, title_string = caption_string, file_sub_label = file_sub_label, plot_name_extension = plot_name_extension, output.directory.short = output.directory.short, image_format = "jpg", ClearPage = ClearPage)
+  
 #### Code below this line probably obsolete ####
-  #source(file.path("estimate-pm25","General_Project_Functions","general_project_functions.R"))
-  #source(file.path("estimate-pm25","General_Project_Functions","merging_data_functions.R"))
-  
-  #csv_file_name_w_path <- file.path("home","rstudio","Processed_Data","ML_input_files","ML_input_PM25_Step4_part_f_de_duplicated_aves.csv")
-  #full_df <- read.csv(csv_file_name_w_path)
-  
-  #processed_data_version <- define_study_constants.fn("processed_data_version")
-  
-  #sub_folder <- "ML_input_files"
-  #ML_input_files <- c(paste("ML_input_PM25_Step4_part_",processed_data_version,"_de_duplicated_aves",sep = ""),paste("ML_input_PM25_Step4_part_",processed_data_version,"_de_duplicated_aves_prioritize_24hr_obs",sep = ""))#, "ML_input_CountyCentroid_Locations_Dates_2008-01-01to2018-12-31")
-  #file_i <- 1
-  #full_df <- read.csv(file.path(define_file_paths.fn("ProcessedData.directory"),sub_folder,paste(ML_input_files[file_i],".csv",sep = "")),header=TRUE) # load the file
-  #full_df <- read.csv(file.path(define_file_paths.fn("ProcessedData.directory"),"ML_input_files",paste(ML_input_files[file_i],".csv",sep = "")),header=TRUE) # load the file
-  #full_df <- read.csv(file.path(define_file_paths.fn("ProcessedData.directory"),"ML_input_files",paste("ML_input_PM25_Step4_part_",processed_data_version,"_de_duplicated_aves.csv",sep = "")),header=TRUE) # load the file
-  #full_df <- read.csv(file.path(define_file_paths.fn("ProcessedData.directory"),"ML_input_files","ML_input_PM25_Step4_part_f_de_duplicated_aves.csv"),header=TRUE) # load the file
-  
-  #map_point_values.fn <- function(this_df, var_interest, cut_point_scale = "PM2.5_Obs", output.directory, file_sub_label, plot_name_extension = plot_name_extension, study_states_abbrev,this_datum, title_string, ClearPage = FALSE, Cut_points_set = FALSE, color_cut_points = NA, color_vec = NA, LatexFileName) { # plot points of observations on map and color points by concentration
-  #} # end of map_point_values.fn function
+  #LaTex_code_4_figure.fn(LatexFileName = LatexFileName, title_string = title_string, file_sub_label = file_sub_label, plot_name_extension = plot_name_extension, output.directory.short = output.directory.short, image_format = "jpg", ClearPage = ClearPage)
   
